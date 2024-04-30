ee8397ae15974c086d991ec44b77a6ebc918cebc
luisagcenteno84 <luisagcenteno@gmail.com>
2024-04-30 05:53:24 -0700

    adding langchain integration

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 import chainlit as cl
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.messages import HumanMessage
+from langchain_core.prompts import PromptTemplate
 
-def on_start:
+chat = GoogleGenerativeAI(model="models/gemini-pro", temperature=0.2)
 
+@cl.on_chat_start
+def on_start():
+    return ""
 
-def on_message:
+@cl.on_message
+async def on_message(message: cl.Message):
+
+    prompt_template = PromptTemplate.from_template(
+        ""
+    )
+
+    response = chat.invoke(
+        [
+            HumanMessage(content=message.content)
+        ]
+    )
+
+    answer = cl.Message(content=response)
+
+    await answer.send()
+    
